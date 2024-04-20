37b8dc238cd7123401a039227c649d80ca03f73a
yumekowq <3263737023@qq.com>
2024-04-20 18:43:08 +0800

    Site updated: 2024-04-20 18:43:05

--- js/randombg.js
+++ js/randombg.js
@@ -1,8 +1,8 @@
 //随机背景图片数组,图片可以换成图床链接，注意最后一条后面不要有逗号
 var backimg =[
-  "url(https://s2.loli.net/2024/04/15/wxdfm92MQIp86y4.jpg)",
-  "url(https://s2.loli.net/2024/04/15/Solxb3VdXNJgPnz.jpg)",
-  "url(https://s2.loli.net/2024/04/15/zOmYvKEJsukaRy8.jpg)",
+  "url(https://s2.loli.net/2024/04/20/mqfQEJpGW61tSxs.jpg)",
+  "url(https://s2.loli.net/2024/04/20/qzjL7opS9QA6mxX.jpg)",
+  "url(https://s2.loli.net/2024/04/20/SNxOKpVjX9B2TkH.jpg)",
   "url(https://s2.loli.net/2024/04/15/lrpzkBInuHLRbwm.jpg)"
   ];
   //获取背景图片总数，生成随机数
@@ -11,9 +11,9 @@ var backimg =[
   document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
   //随机banner数组,图片可以换成图床链接，注意最后一条后面不要有逗号
   var bannerimg =[
-    "url(https://s2.loli.net/2024/04/15/wxdfm92MQIp86y4.jpg)",
-    "url(https://s2.loli.net/2024/04/15/Solxb3VdXNJgPnz.jpg)",
-    "url(https://s2.loli.net/2024/04/15/zOmYvKEJsukaRy8.jpg)",
+    "url(https://s2.loli.net/2024/04/20/mqfQEJpGW61tSxs.jpg)",
+    "url(https://s2.loli.net/2024/04/20/qzjL7opS9QA6mxX.jpg)",
+    "url(https://s2.loli.net/2024/04/20/SNxOKpVjX9B2TkH.jpg)",
     "url(https://s2.loli.net/2024/04/15/lrpzkBInuHLRbwm.jpg)"
   ];
   //获取banner图片总数，生成随机数
